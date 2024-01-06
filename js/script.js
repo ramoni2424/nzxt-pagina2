93b2e5ce2705332fb286184cd59ea15e251ab3c3
ramoni2424 <72168270+ramoni2424@users.noreply.github.com>
2024-01-06 23:17:56 +0100

    .

--- js/script.js
+++ js/script.js
@@ -22,13 +22,13 @@ hands[1].setAttribute('to', shifter(minuteAngle + 360));
 hands[2].setAttribute('from', shifter(hoursAngle));
 hands[2].setAttribute('to', shifter(hoursAngle + 360));
 
-for(var i = 1; i <= 12; i++) {
+for (var i = 1; i <= 12; i++) {
   var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
   el.setAttribute('x1', '100');
   el.setAttribute('y1', '30');
   el.setAttribute('x2', '100');
   el.setAttribute('y2', '40');
-  el.setAttribute('transform', 'rotate(' + (i*360/12) + ' 100 100)');
+  el.setAttribute('transform', 'rotate(' + (i * 360 / 12) + ' 100 100)');
   el.setAttribute('style', 'stroke: #ffffff;');
   document.querySelector('svg').appendChild(el);
 }
